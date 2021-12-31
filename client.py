f3ec77005f2f85a192efe7a53b895d2202172d8a
XChronosCS <33588396+XChronosCS@users.noreply.github.com>
2021-12-31 13:19:44 -0500

    12/31 -> Implemented Google Sheet API to get data from google sheets

--- client.py
+++ client.py
@@ -1,18 +1,40 @@
-# bot.py
+# client.py
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+from TownEvents import *
+from DataGet import *
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+print(TOKEN)
 
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord! Bababoee')
+bot = commands.Bot(command_prefix='!')
 
 
-client.run(TOKEN)
+@bot.command(name='towneventtest')
+async def towneventtest(ctx):
+    result = get_town_event()
+    await ctx.send(result[0])
+    await ctx.send(result[1])
+
+
+@bot.command(name='ability')
+async def ability(ctx, *arg):
+    arg_full = ' '.join(arg)
+    result = get_ability_data(arg_full)
+    for x in result:
+        await ctx.send(x)
+
+
+@bot.command(name='feature')
+async def feature(ctx, *arg):
+    arg_full = ' '.join(arg)
+    result = get_feature_data(arg_full)
+    for x in result:
+        await ctx.send(x)
+
+
+bot.run(TOKEN)
