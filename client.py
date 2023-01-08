38970a9c76985797761d862c416c925edfe39cdb
XChronosCS <33588396+XChronosCS@users.noreply.github.com>
2023-01-08 17:40:39 -0500

    Switching to Porybot1 Token

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ from RollingCommands import *
 from UIElements import *
 
 load_dotenv()
-TOKEN = os.getenv('TEST_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
 print(TOKEN)
 intents = discord.Intents.default()
 intents.message_content = True
