4a6d1fbe622523ea30130c8cfd395eeb284e4d2e
XChronosCS <33588396+XChronosCS@users.noreply.github.com>
2023-01-09 12:44:47 -0500

    Attempting to have multiple selects in view.

--- UIElements.py
+++ UIElements.py
@@ -120,30 +120,33 @@ class PXPCalcView(discord.ui.View):
     @discord.ui.select(custom_id="Encounter Type", placeholder="Select Encounter Type",
                        options=[discord.SelectOption(label=name[0], value=name[1]) for name in
                        [("Exploration Base", "3"), ("Exploration Training Intent", "5"), ("Raid", "5"),
-                        ("Adventure Trial Pass", "4"), ("Adventure Trial Fail", "2"), ("Clash Encounter", "3"), ("Rescue Encounter", "3"), ("Request Encounter", "5"), ("Gauntlet Encounter", "5")]], max_values=1, row=1)
+                        ("Adventure Trial Pass", "4"), ("Adventure Trial Fail", "2"), ("Clash Encounter", "3"), ("Rescue Encounter", "3"), ("Request Encounter", "5"), ("Gauntlet Encounter", "5")]], max_values=1, row=0)
     async def select(self, interaction: discord.Interaction, select: discord.ui.Select):
         self.encounter_type = int(select.values[0])
         self.stop()
+        await interaction.response.defer()
 
     @discord.ui.select(custom_id="Number Players", placeholder="# Players",
-                       options=[discord.SelectOption(label=str(i+1), value=str(i+1)) for i in range(4)], max_values=1, row=2)
+                       options=[discord.SelectOption(label=str(i+1), value=str(i+1)) for i in range(4)], max_values=1, row=1)
     async def select(self, interaction: discord.Interaction, select: discord.ui.Select):
         self.num_players = int(select.values[0])
         self.stop()
+        await interaction.response.defer()
 
     @discord.ui.select(custom_id="Doubling Rewards?", placeholder="Choose yes or no",
                        options=[discord.SelectOption(label=i, value=i) for i in ["Yes", "No"]],
-                       max_values=1, row=3)
+                       max_values=1, row=2)
     async def select(self, interaction: discord.Interaction, select: discord.ui.Select):
         self.doubled = True if select.values[0] == "Yes" else False
         self.stop()
+        await interaction.response.defer()
 
     async def on_timeout(self) -> None:
         await self.message.channel.send("Timedout")
         await self.disable_all_items()
 
     @discord.ui.button(label="Press to Continue",
-                       style=discord.ButtonStyle.success)
+                       style=discord.ButtonStyle.success, row=3)
     async def submit(self, interaction: discord.Interaction, button: discord.ui.Button):
         op_modal = PXPCalcModal()
         op_modal.assign_req(np=self.num_players, et=self.encounter_type, d=self.doubled)
