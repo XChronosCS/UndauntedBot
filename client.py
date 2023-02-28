1b3befee5bb7df276f7cde0d04bb5d9bee2fb10d
XChronosCS <33588396+XChronosCS@users.noreply.github.com>
2023-02-28 16:44:01 -0500

    Fixing Issue with /details command which was not letting it properly work with 2 word pokemon names.

--- client.py
+++ client.py
@@ -166,11 +166,11 @@ async def cookie(ctx, person: discord.Member = None):
 
 
 @bot.command(name='details')
-async def details(ctx, arg=None):
+async def details(ctx, *arg):
     if arg is None:
         await ctx.send(roll_details())
     else:
-        if type(arg) is list:
+        if type(arg) is tuple:
             arg = " ".join(arg)
         try:
             await ctx.send(roll_mon_details(ALLPOKEMON[arg.upper()]))
